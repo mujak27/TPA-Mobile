25d938feaf7c40c8f3cd0bb0bead67d225ccc6da
Vionny <senvionny@gmail.com>
2023-01-04 18:36:28 +0700

    new senderhome

---
+++ app/src/main/java/edu/bluejack22_1/beefood/user/SenderHome.kt
@@ -0,0 +1,12 @@
+package edu.bluejack22_1.beefood.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.bluejack22_1.beefood.R
+
+class SenderHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sender_home)
+    }
+}
